3cd2137c58f301ced29f7319aab153beb1ca97ea
Matilda Mared <matildamared@live.se>
2024-12-11 08:25:06 +0100

    solve day 11 part 1

--- 11/index.ts
+++ 11/index.ts
@@ -3,11 +3,34 @@ import * as fs from "fs";
 // 🎄 🎅 Advent of Code 2024 Day 11 🎅 🎄
 
 const input = fs.readFileSync("input.txt", "utf8");
-const lines = input.split("\n");
+const stones = input.split(" ").map((stone) => parseInt(stone));
 
 // 🍬 🍭 Part 1 🍭 🍬
 
-console.log(`🎄 🎅 The answer to part 1 is: ${undefined} 🍬 🍭`);
+const numberOfBlinks = 25;
+
+for (let i = 1; i <= numberOfBlinks; i++) {
+	for (let stoneIndex = 0; stoneIndex < stones.length; stoneIndex++) {
+		let currentStone = stones[stoneIndex];
+		const newStones: number[] = [];
+
+		if (currentStone === 0) {
+			newStones.push(1);
+		} else if (currentStone.toString().length % 2 === 0) {
+			const half = currentStone.toString().length / 2;
+			const firstStone = parseInt(currentStone.toString().slice(0, half));
+			const secondStone = parseInt(currentStone.toString().slice(half));
+			newStones.push(firstStone, secondStone);
+		} else {
+			newStones.push(currentStone * 2024);
+		}
+
+		stones.splice(stoneIndex, 1, ...newStones);
+		stoneIndex += newStones.length - 1;
+	}
+}
+
+console.log(`🎄 🎅 The answer to part 1 is: ${stones.length} 🍬 🍭`);
 
 // 🍬 🍭 Part 2 🍭 🍬
 
