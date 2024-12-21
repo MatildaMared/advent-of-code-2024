c9aeab385503dedacbafc530b31a1312823cc371
Matilda Mared <matildamared@live.se>
2024-12-21 18:20:59 +0100

    prepare for day 21

---
+++ 21/index.ts
@@ -0,0 +1,14 @@
+import * as fs from "fs";
+
+// 🎄 🎅 Advent of Code 2024 Day 21 🎅 🎄
+
+const input = fs.readFileSync("input.txt", "utf8");
+const lines = input.split("\n");
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 1 is: ${undefined} 🍬 🍭`);
+
+// 🍬 🍭 Part 2 🍭 🍬
+
+console.log(`🎄 🎅 The answer to part 2 is: ${undefined} 🍬 🍭`);
